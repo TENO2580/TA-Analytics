772b839ded4b8f73b1ea8bee307de25326faafa3
TENO G TERRY <149796900+TENO2580@users.noreply.github.com>
2026-05-19 17:41:51 +0530

    Delete tenowebsite.html directory

--- tenowebsite.html/rfm_gen.py
+++
@@ -1,4 +0,0 @@
-html = open("rfm_part1.html","w",encoding="utf-8")
-html.write("<!DOCTYPE html><html><head><title>RFM</title></head><body><h1>RFM Tool</h1></body></html>")
-html.close()
-print("done")
